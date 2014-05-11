babd0de19e18c1a9686c6053624ab8483fbd3165
yrahan <yacine.bourkeb@gmail.com>
2014-05-11 15:48:15 +0200

    plot 1,2,3 done, starting plot4

--- plot1.R
+++ plot1.R
@@ -1,10 +1,11 @@
-#consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";")
+# read data
 consumption <- read.table("household_power_consumption.txt", header=TRUE, sep=";", 
                           na.string="?")
 
 # convert column Date to Date format
 consumption$Date <- as.Date(as.character(consumption$Date),format="%d/%m/%Y")
 
+# determine the perimeter of selection 
 datesPerimeter <- c(as.Date("2007-02-01" , format="%Y-%m-%d"),
                     as.Date("2007-02-02" , format="%Y-%m-%d"))
 
@@ -12,8 +13,8 @@ datesPerimeter <- c(as.Date("2007-02-01" , format="%Y-%m-%d"),
 selection <- consumption[consumption$Date %in% datesPerimeter,]
 
 # open png device
-png(file="./figure/plot1.png",width=480,height=480)
-#?hist
+png(file="plot1.png",width=480,height=480)
+# make the figure
 hist(as.numeric(as.character(selection$Global_active_power)), 
      xlab="Global Active Power (kilowatts)", main="Global Active Power",
      col="red")
--- plot4.R
+++ plot4.R
@@ -12,14 +12,28 @@ datesPerimeter <- c(as.Date("2007-02-01" , format="%Y-%m-%d"),
 selection <- consumption[consumption$Date %in% datesPerimeter,]
 
 # open png device
-png(file="plot2.png",width=480,height=480)
+png(file="plot3.png",width=480,height=480)
 # make the figure
 # Sys.setlocale(category = "LC_TIME", locale = "C")
 plot(strptime(paste(selection$Date,selection$Time),format="%Y-%m-%d %H:%M:%S"),
-     selection$Global_active_power,
+     selection$Sub_metering_1,
      xlab="",
-     ylab="Global Active Power (kilowatts)",
-     type="l")
+     ylab="Energy sub metering",
+     type="l"
+     )
+lines(strptime(paste(selection$Date,selection$Time),format="%Y-%m-%d %H:%M:%S"),
+     selection$Sub_metering_2,
+     col="red"
+     )
+lines(strptime(paste(selection$Date,selection$Time),format="%Y-%m-%d %H:%M:%S"),
+      selection$Sub_metering_3,
+      col="blue"
+)
+legend("topright", 
+       lty=c(rep(1,3)), 
+       col = c("black","red", "blue"), 
+       legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3")
+       )
 # close file
 dev.off()
 # Sys.setlocale(category = "LC_TIME", locale = "")
