5e7860f745b1a8ae73de0d9f2026a2730217947f
yrahan <yacine.bourkeb@gmail.com>
2014-05-25 18:36:28 +0200

    do question 1

---
+++ plot1.R
@@ -0,0 +1,25 @@
+# Get Data
+zipfile <- "data.zip"
+# avoid unecessary downloads
+if (!file.exists(zipfile)){
+    fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip"
+    download.file(fileUrl, destfile = zipfile, method = "curl")
+
+}
+unzip(zipfile)
+
+# read data
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+str(NEI)
+str(SCC)
+
+table(NEI$year)
+# open png device
+png(file="plot1.png",width=480,height=480)
+barplot(tapply(NEI$Emissions,NEI$year,sum),xlab="year",ylab="tons", 
+        main="Total PM2.5 emission from all sources \nfor each of the years 1999, 2002, 2005, and 2008.")
+# close file
+dev.off()
